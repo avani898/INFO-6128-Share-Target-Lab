fd7c97d5b078b9f7dea10a81e354df5cd39c0ce8
avani898 <94886488+avani898@users.noreply.github.com>
2022-01-27 17:49:03 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const parsedUrl = new URL(window.location);
+    // searchParams.get() will properly handle decoding the values.
+    document.getElementById('title')  =parsedUrl.searchParams.get('title');
+    document.getElementById('text')= parsedUrl.searchParams.get('text');
+    document.getElementById('url')= parsedUrl.searchParams.get('url');
+  });
