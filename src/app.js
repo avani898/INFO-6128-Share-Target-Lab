400089d0b0a18b3249eacd04202589a083c31f38
avani898 <94886488+avani898@users.noreply.github.com>
2022-01-27 20:16:20 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,1 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const parsedUrl = new URL(window.location);
-    // searchParams.get() will properly handle decoding the values.
-    document.getElementById('title').value;
-    document.getElementById('text').value;
-    document.getElementById('url').value;
-  });
+
